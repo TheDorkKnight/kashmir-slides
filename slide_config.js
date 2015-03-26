596bd3d2860bab29b5142f7bf8f47dd231db20d7
Jake Molnar <jake.molnar@gmail.com>
2015-03-25 17:22:30 -0700

    First pages commit

---
+++ slide_config.js
@@ -0,0 +1,45 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Azad Kashmir<br>Occupied or Free?',
+    subtitle: 'Differring Perspectives on a Border',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/jm_icon_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jake Molnar',
+    company: 'Computer Science',//company: 'Software Developer, jakemolnar.com',
+    twitter: '@losttodorkness',
+    www: 'http://www.jakemolnar.com',
+    github: 'http://github.com/TheDorkKnight'
+  }, {
+    name: 'Sean Callegari',
+    company: 'Psychology'
+  }, {
+    name: 'Nathan Laronde',
+    company: 'Anthropology'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
