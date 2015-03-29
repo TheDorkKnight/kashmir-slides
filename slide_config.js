eb8453b2082f81709ebbf49679d3bd2bb74ab518
Jake Molnar <jake.molnar@gmail.com>
2015-03-29 12:32:42 -0700

    Change subtitle

--- slide_config.js
+++ slide_config.js
@@ -2,7 +2,7 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Azad Kashmir<br>Occupied or Free?',
-    subtitle: 'Differring Perspectives on a Border',
+    subtitle: 'Perspectives on a Border',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
